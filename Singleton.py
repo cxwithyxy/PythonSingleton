6a3b83dccb90548a2324914d3c9c96dcee9c5008
chenxuan <523764720@qq.com>
2019-07-04 13:54:45 +0800

    修改了一下线程锁, 不知道会不会还有问题

--- Singleton.py
+++ Singleton.py
@@ -1,14 +1,14 @@
 #coding:utf-8
 import threading
 class Singleton(object):
-    _instance_lock = threading.Lock()
 
     def __new__(cls, *args, **kwargs):
+        lock = threading.Lock()
+        lock.acquire()
         if not hasattr(cls, "_instance"):
-            with cls._instance_lock:
-                if not hasattr(cls, "_instance"):
-                    cls._instance = object.__new__(cls)
-                    cls._instance.__Singleton_Init__(*args, **kwargs)
+            cls._instance = object.__new__(cls)
+            cls._instance.__Singleton_Init__(*args, **kwargs)
+        lock.release()
         return cls._instance
 
     def __Singleton_Init__(self):
